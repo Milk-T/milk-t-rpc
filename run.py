9f24456bccd07635090902cbdc9cb595ff27f197
hugo <cwang.6379@gmail.com>
2023-12-04 01:18:28 +0800

    log event

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import sys
 import logging
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
 
 import vnpy.trader.ui  # noqa
 from vnpy_ctp import CtpGateway
 from vnpy.event import EventEngine, Event
 from vnpy.trader.engine import MainEngine
-from vnpy.trader.event import EVENT_LOG
-from vnpy_rpcservice.rpc_service.engine import EVENT_RPC_LOG
 from vnpy_webtrader import WebEngine
 from vnpy_algotrading import AlgoTradingApp
 
@@ -15,22 +16,34 @@ logger = logging.getLogger("Milk-T-RPC")
 logger.setLevel(logging.DEBUG)
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 stdout_handler.setFormatter(formatter)
 logger.addHandler(stdout_handler)
 
 
+def to_dict(o: dataclass) -> dict:
+    """将对象转换为字典"""
+    data: dict = {}
+    for k, v in o.__dict__.items():
+        if isinstance(v, Enum):
+            data[k] = v.value
+        elif isinstance(v, datetime):
+            data[k] = str(v)
+        else:
+            data[k] = v
+    return data
+
+
 def process_log_event(event: Event):
     """"""
-    log = event.data
-    logger.info("Log event:%s\t%s", log.time, log.msg)
+    logger.info("event=%s", to_dict(event))
 
 
 def main():
     """Start VN Trader"""
     event_engine = EventEngine()
-    event_engine.register(EVENT_LOG, process_log_event)
-    event_engine.register(EVENT_RPC_LOG, process_log_event)
+    event_engine.register_general(process_log_event)
 
     main_engine = MainEngine(event_engine)
 
